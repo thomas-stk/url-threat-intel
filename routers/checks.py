fe90b30cda27b3153f43a0152fbd6b418a7bb86c
Thomas <thomas.stk.7@gmail.com>
2026-05-29 13:25:45 +0100

    feat: Added CORS and done input validation

--- routers/checks.py
+++ routers/checks.py
@@ -3,6 +3,7 @@ from slowapi import Limiter
 from slowapi.util import get_remote_address
 from services.abuseipdb import check_abuseipdb
 from services.virustotal import check_virustotal_url
+from pydantic import IPvAnyAddress, AnyHttpUrl
 
 
 limiter = Limiter(key_func=get_remote_address)
@@ -10,13 +11,13 @@ router = APIRouter()
 
 @router.get("/check/ip")
 @limiter.limit("10/minute")
-async def check_ip(request: Request, ip: str):
-    result = await check_abuseipdb(ip)
-    return result  
+async def check_ip(request: Request, ip: IPvAnyAddress):
+    result = await check_abuseipdb(str(ip))
+    return result
 
 @router.get("/check/url")
 @limiter.limit("10/minute")
-async def check_url(request: Request, url: str):
-    result = await check_virustotal_url(url)
+async def check_url(request: Request, url: AnyHttpUrl):
+    result = await check_virustotal_url(str(url))
     return result   
 
